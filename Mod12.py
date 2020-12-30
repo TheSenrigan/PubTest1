a0510a87699f8112330f035c0c9300c1ce1028a6
TheSenrigan <32772243+TheSenrigan@users.noreply.github.com>
2020-12-30 12:32:28 +0400

    Add files via upload
    Итоговое задание 12 модуль Тимофеев QAP-10

---
+++ Mod12.py
@@ -0,0 +1,10 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0} 
+money=float(input('Введите сумму счёта:'))
+deposit=[]
+deposit.append(per_cent['ТКБ']*money/100)
+deposit.append(per_cent['СКБ']*money/100)
+deposit.append(per_cent['ВТБ']*money/100)
+deposit.append(per_cent['СБЕР']*money/100)
+deposit.sort()
+print(deposit)
+print('Максимальная сумма:'+str(round(deposit[-1],2)))
